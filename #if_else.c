94f9740de9d73a87d6b94b8dd13a5c0415b0a194
Sansardar <rockstarsaanu95sardar@gmail.com>
2024-11-20 13:47:26 +0530

    Create #if_else.c
    if-else statement.
    * If else statement is a decision making statement.
    * The if-else statement is used to perform logical tests and then, take one of the two possible actions
    depending on the result of the test.
    * The if-else statement has two block of statement.
    * If block : The block of statements followed by if
    statement are executed when the given condition (in if
    block) is true.
    * else block : The block of statements followed by else are
    executed when the given condition (in if block) is false.
    * The if-else statement either execute if block or it execute else
    block.
    * If - else both statements never ends with semicolon.
    
    * The general form of if-else statement is.
    SYNTAX:
    if (test expression)
    {
    Statement_block1 ;
     // Executed when expression is true
    }
    else
    {
    Statement_block2 ;
     // Executed when expression is false
    }
    statement-x ;
    
    Explanation:
    * If the test expression is true, then the Statement-block1 which immediately follows the if, is executed
    otherwise, the Statement block2 is executed.

---
+++ #if_else.c
@@ -0,0 +1,12 @@
+                                                                            // program to check whether the number is even or odd.
+#include <stdio.h>
+main()
+{
+int n;
+printf ("Enter a number: ");
+scanf ("%d", &n);
+if (n%2==0)
+printf ("Number entered is EVEN");
+else
+printf ("Number entered is ODD");
+}
